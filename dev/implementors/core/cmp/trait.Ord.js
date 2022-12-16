cc0f93d7f57073938212a02cdd610a077ecd4c66
sylvestre <sylvestre@users.noreply.github.com>
2022-12-16 02:39:27 +0000

    deploy: 2ed80ff15f550c1004dff01f0f83a4c150bb7e5b

--- dev/implementors/core/cmp/trait.Ord.js
+++ dev/implementors/core/cmp/trait.Ord.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"uu_cp":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"uu_cp/enum.Attribute.html\" title=\"enum uu_cp::Attribute\">Attribute</a>"]],
-"uucore":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"uucore/ranges/struct.Range.html\" title=\"struct uucore::ranges::Range\">Range</a>"]]
+"uu_cp":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"uu_cp/enum.Attribute.html\" title=\"enum uu_cp::Attribute\">Attribute</a>"]],
+"uucore":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"uucore/ranges/struct.Range.html\" title=\"struct uucore::ranges::Range\">Range</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
