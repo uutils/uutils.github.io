d236817d0326b54727fcf40ab495737f665b8c58
sylvestre <sylvestre@users.noreply.github.com>
2023-03-02 02:58:35 +0000

    deploy: f0e8d44e6e64ee3354f95864c0a25b74cf1c1151

--- dev/uu_tail/args/sidebar-items.js
+++ dev/uu_tail/args/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["FilterMode",""],["FollowMode",""],["Signum",""],["VerificationResult",""]],"fn":[["arg_iterate",""],["parse_args",""],["uu_app",""]],"mod":[["options",""]],"struct":[["Settings",""]]};
+window.SIDEBAR_ITEMS = {"enum":[["FilterMode",""],["FollowMode",""],["Signum",""],["VerificationResult",""]],"fn":[["parse_args",""],["parse_obsolete",""],["uu_app",""]],"mod":[["options",""]],"struct":[["Settings",""]]};
