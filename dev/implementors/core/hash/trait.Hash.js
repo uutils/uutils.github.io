41434b5339fe3bdbde8f64b4d0e609b69321bcea
sylvestre <sylvestre@users.noreply.github.com>
2023-03-29 02:48:37 +0000

    deploy: f0e8d44e6e64ee3354f95864c0a25b74cf1c1151

--- dev/implementors/core/hash/trait.Hash.js
+++ dev/implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"uucore":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"uucore/fs/struct.FileInformation.html\" title=\"struct uucore::fs::FileInformation\">FileInformation</a>"]]
+"uucore":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"uucore/fs/struct.FileInformation.html\" title=\"struct uucore::fs::FileInformation\">FileInformation</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
