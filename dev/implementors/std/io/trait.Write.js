e590546fcdf98cd1c018f5b0164d343a2c4b46d6
sylvestre <sylvestre@users.noreply.github.com>
2023-06-02 02:51:36 +0000

    deploy: f0e8d44e6e64ee3354f95864c0a25b74cf1c1151

--- dev/implementors/std/io/trait.Write.js
+++ dev/implementors/std/io/trait.Write.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"uucore":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"uucore/sum/struct.DigestWriter.html\" title=\"struct uucore::sum::DigestWriter\">DigestWriter</a>&lt;'a&gt;"]]
+"uucore":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"uucore/sum/struct.DigestWriter.html\" title=\"struct uucore::sum::DigestWriter\">DigestWriter</a>&lt;'a&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
